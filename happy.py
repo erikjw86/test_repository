c43f371fa096731b681fd8bef8a7f9371293bb94
Erik Winkler <ErikWinkler@Eriks-MacBook-Pro.local>
2021-12-02 00:56:23 -0500

    added python test program

---
+++ happy.py
@@ -0,0 +1,15 @@
+def test_repo_func():
+	'''
+	test function for github learning
+	'''
+
+	feeling=raw_input('How are you feeling today? ')
+
+	if feeling == 'happy':
+		print('Great to hear :)')
+	elif feeling == 'sad':
+		print('Cheer up!')
+	else:
+		print('I am not a complex program. I will only respond to happy or sad')
+
+test_repo_func()
